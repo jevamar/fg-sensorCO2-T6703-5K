715d531d20317c19226cf2a934a41db79c58fb71
jevamar <kemdeveloper@gmail.com>
2024-10-10 22:07:14 +0700

    sonar sensor

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,16 @@
 // #include <Arduino.h>
 #include <Esp.h>
 
+#define PIN_TRIG D2
+#define PIN_ECHO D3
 #define LED_PIN D1
 #define BLINK_DELAY 1000
 
+unsigned long timerLed = 0;
+unsigned long duration, cm;
+
 // put function declarations here:
-int myFunction(int, int);
+void handleSonar();
 
 void setup() {
   Serial.begin(9600);
@@ -13,19 +18,45 @@ void setup() {
   // int result = myFunction(2, 3);
   pinMode(LED_PIN, OUTPUT);
   pinMode(LED_BUILTIN, OUTPUT);
-  
+  pinMode(PIN_TRIG, OUTPUT);
+  delay(300);
 }
 
 void loop() {
-  
-  Serial.println("Hello World!");
-  // delay(1000);
-  // // put your main code here, to run repeatedly:
-  digitalWrite(LED_PIN, HIGH);
-  digitalWrite(LED_BUILTIN, HIGH);
+  handleSonar();
 
-  delay(BLINK_DELAY);
-  digitalWrite(LED_PIN, LOW);
-  digitalWrite(LED_BUILTIN, LOW);
-  delay(BLINK_DELAY);
+  if(millis() - timerLed >= 2000) {
+    timerLed = millis();
+    
+    // Serial.print("LED: turn on. ");
+    // Serial.println(timerLed);
+    
+    digitalWrite(LED_PIN, HIGH);
+    digitalWrite(LED_BUILTIN, HIGH);
+
+    delay(BLINK_DELAY);
+    digitalWrite(LED_PIN, LOW);
+    digitalWrite(LED_BUILTIN, LOW);
+    // delay(BLINK_DELAY);
+  }
+}
+
+void handleSonar() 
+{
+  // Создаем короткий импульс длительностью 5 микросекунд.
+  digitalWrite(PIN_TRIG, LOW);
+  delayMicroseconds(5);
+  digitalWrite(PIN_TRIG, HIGH);
+  // Установим высокий уровень сигнала
+  delayMicroseconds(10);
+  digitalWrite(PIN_TRIG, LOW);
+  //  Определяем задержку сигнала
+  duration = pulseIn(PIN_ECHO, HIGH);
+  // Преобразуем время задержки в расстояние
+  cm = (duration / 2) / 29.1;
+  Serial.print("Расстояние до объекта: ");
+  Serial.print(cm);
+  Serial.println(" см.");
+  // Задержка между измерениями
+  delay(250);
 }
